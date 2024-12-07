18303f762fbe69f8387515a0c619d8988c7aa906
JoyBlaster <s10255718@connect.np.edu.sg>
2024-12-08 01:05:11 +0800

    Rover Fixing Function
    - Added Rover Fixing Function (In a separate scene)
    - Drag over the right parts to the right areas
    - Could probably have some sort of image of a rover blueprint to allow player to understand where each part goes
    - When rover is fully fixed, a script logs that it is fixed in Debug log (can do something with this in the future)
    - As of now, there's only 3 parts to be fixed. I can add more parts in the future if needed.

---
+++ Assets/Shaun's Function/RoverFixed.cs
@@ -0,0 +1,52 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoverFixed : MonoBehaviour
+{
+    // Bool to check if each part has been fixed yet
+    public bool wheelFixed = false;
+    public bool cameraFixed = false;
+    public bool panelFixed = false;
+
+    // When fixed wheel
+    public void FixWheel()
+    {
+        wheelFixed = true;
+        Debug.Log("Wheel fixed");
+        CheckProgress();
+    }
+    // When fixed camera
+    public void FixCam()
+    {
+        cameraFixed = true;
+        Debug.Log("Cam fixed");
+        CheckProgress();
+    }
+    // When fixed panel
+    public void FixPanel()
+    {
+        panelFixed = true;
+        Debug.Log("Panel fixed");
+        CheckProgress();
+    }
+
+    void CheckProgress()
+    {
+        if (wheelFixed)
+        { 
+            if (cameraFixed)
+            {
+                if (panelFixed)
+                {
+                    FixComplete();
+                }
+            }
+        }
+    }
+
+    void FixComplete()
+    {
+        Debug.Log("Nice.");
+    }
+}
