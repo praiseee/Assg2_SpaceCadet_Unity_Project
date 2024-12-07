abb83b14046bc7b7d7b75580806338dd745d0638
JacieThoo <s10260236@connect.np.edu.sg>
2024-12-07 16:30:01 +0800

    Added Dialogue Script
    To be linked with MissionManager

---
+++ Assets/Scripts/DialogueSystem.cs
@@ -0,0 +1,73 @@
+/*
+ * Author: Jacie Thoo Yixuan
+ * Date: 7/12/2024
+ * Description: Handles audio dialogue and subtitles
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class DialogueSystem : MonoBehaviour
+{
+    [System.Serializable]
+    public class DialogueLine
+    {
+        public string name;
+        public string subtitleText;
+        public AudioClip audioClip;
+    }
+
+    //Store dialogues
+    public List<DialogueLine> dialogueLines;
+
+    // Audio
+    public AudioSource audioSource;
+
+    // Subs
+    public TextMeshProUGUI subtitleTextUI;
+
+    // Time before next line
+    public float delayBetweenLines = 0.5f;
+
+    // What line it is now
+    private int currentLineIndex = 0;
+
+    /// <summary>
+    /// Play specific dialogue lines 
+    /// </summary>
+    public void PlayDialogueRange(int startIndex, int endIndex)
+    {
+        if (startIndex < 0 || endIndex >= dialogueLines.Count || startIndex > endIndex)
+        {
+            Debug.LogError("Invalid dialogue range.");
+            return;
+        }
+        StopAllCoroutines();
+        StartCoroutine(PlayDialogue(startIndex, endIndex));
+    }
+
+    private IEnumerator PlayDialogue(int startIndex, int endIndex)
+    {
+        for (int i = startIndex; i <= endIndex; i++)
+        {
+            DialogueLine line = dialogueLines[i];
+            audioSource.clip = line.audioClip;
+            audioSource.Play();
+
+            if (subtitleTextUI != null)
+                subtitleTextUI.text = line.subtitleText;
+
+            yield return new WaitForSeconds(line.audioClip.length + delayBetweenLines);
+        }
+
+        if (subtitleTextUI != null)
+            subtitleTextUI.text = "";
+    }
+
+    public void PlaySingleLine(int index)
+    {
+        PlayDialogueRange(index, index);
+    }
+}
